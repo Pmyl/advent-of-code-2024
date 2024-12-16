20680de6bda1d68ba493a1df542f3b111336dd5c
Giulio Caprino <giulio.caprino@dataglide.co>
2024-12-16 23:11:22 +0000

    day16 speed up by not storing the whole path but by marking the maze

--- src/days/day16/mod.rs
+++ src/days/day16/mod.rs
@@ -24,7 +24,7 @@ struct ReindeerMaze {
 enum Cell {
     Free,
     Wall,
-    Visited([usize; 4]),
+    Visited([usize; 4], Vec<usize>),
 }
 
 impl ReindeerMaze {
@@ -67,90 +67,125 @@ impl ReindeerMaze {
     }
 
     fn count_tiles_of_lowest_score_possible(&self) -> usize {
-        HashSet::<Position>::from_iter(self.all_lowest_score_paths().1.into_iter().flatten()).len()
+        self.all_lowest_score_paths().1
     }
 
-    fn all_lowest_score_paths(&self) -> (usize, Vec<Vec<Position>>) {
+    fn all_lowest_score_paths(&self) -> (usize, usize) {
         let width = self.maze[0].len();
         let height = self.maze.len();
 
-        let mut min_score: (usize, Vec<Vec<Position>>) = (usize::MAX, vec![]);
+        let mut min_score: (usize, Vec<usize>) = (usize::MAX, vec![]);
 
         let mut maze = self.maze.clone();
-        let mut cases = BTreeMap::<usize, Vec<(Position, Direction, Vec<Position>)>>::new();
+
+        struct Case {
+            pos: Position,
+            dir: Direction,
+            ids: Vec<usize>,
+        }
+
+        let mut cases = BTreeMap::<usize, Vec<Case>>::new();
+
+        let mut current_id = 0;
+
         cases.insert(
             0,
-            vec![(
-                self.start.clone(),
-                Direction::Right,
-                vec![self.start.clone()],
-            )],
+            vec![Case {
+                pos: self.start.clone(),
+                dir: Direction::Right,
+                ids: vec![current_id],
+            }],
         );
+
+        current_id += 1;
 
         while let Some((score, sub_cases)) = cases.pop_first() {
             for case in sub_cases {
-                if let Cell::Visited(mut scores) = maze[case.0 .1][case.0 .0] {
-                    scores[direction_to_index(&case.1)] = score;
+                if let Cell::Visited(mut scores, ref mut ids) = maze[case.pos.1][case.pos.0] {
+                    scores[direction_to_index(&case.dir)] = score;
+                    ids.push(case.ids[0]);
                 } else {
                     let mut scores = [usize::MAX; 4];
-                    scores[direction_to_index(&case.1)] = score;
-                    maze[case.0 .1][case.0 .0] = Cell::Visited(scores);
+                    scores[direction_to_index(&case.dir)] = score;
+                    maze[case.pos.1][case.pos.0] = Cell::Visited(scores, vec![case.ids[0]]);
                 }
 
-                let forward_position = case
-                    .0
-                    .move_by(&Distance::from_direction(&case.1), width, height)
-                    .expect("the map has walls all around so it won't ever go out");
-                let mut forward_path = case.2.clone();
-                forward_path.push(forward_position.clone());
-                let case_forward = (score + 1, (forward_position, case.1.clone(), forward_path));
-
-                let turn_left = case.1.turn_left();
+                let turn_left = case.dir.turn_left();
                 let left_position = case
-                    .0
+                    .pos
                     .move_by(&Distance::from_direction(&turn_left), width, height)
                     .expect("the map has walls all around so it won't ever go out");
-                let mut left_path = case.2.clone();
-                left_path.push(left_position.clone());
-                let case_left = (score + 1001, (left_position, turn_left, left_path));
+                let mut left_ids = vec![current_id];
+                left_ids.extend(case.ids.iter());
+                let case_left = (
+                    score + 1001,
+                    Case {
+                        pos: left_position,
+                        dir: turn_left,
+                        ids: left_ids,
+                    },
+                );
+                current_id += 1;
 
-                let turn_right = case.1.turn_right();
+                let turn_right = case.dir.turn_right();
                 let right_position = case
-                    .0
+                    .pos
                     .move_by(&Distance::from_direction(&turn_right), width, height)
                     .expect("the map has walls all around so it won't ever go out");
-                let mut right_path = case.2.clone();
-                right_path.push(right_position.clone());
-                let case_right = (score + 1001, (right_position, turn_right, right_path));
+                let mut right_ids = vec![current_id];
+                right_ids.extend(case.ids.iter());
+                let case_right = (
+                    score + 1001,
+                    Case {
+                        pos: right_position,
+                        dir: turn_right,
+                        ids: right_ids,
+                    },
+                );
+                current_id += 1;
 
-                for next_case in [case_forward, case_left, case_right] {
-                    let next_score = next_case.0;
-                    let next_position = &next_case.1 .0;
-                    let next_direction = &next_case.1 .1;
-                    let next_path = &next_case.1 .2;
+                let forward_position = case
+                    .pos
+                    .move_by(&Distance::from_direction(&case.dir), width, height)
+                    .expect("the map has walls all around so it won't ever go out");
+                let mut forward_ids = vec![current_id];
+                forward_ids.extend(case.ids.iter());
+                let case_forward = (
+                    score + 1,
+                    Case {
+                        pos: forward_position,
+                        dir: case.dir.clone(),
+                        ids: forward_ids,
+                    },
+                );
+                current_id += 1;
+
+                for next_case_with_score in [case_forward, case_left, case_right] {
+                    let next_score = next_case_with_score.0;
+                    let next_case = next_case_with_score.1;
 
                     if next_score > min_score.0 {
                         continue;
                     }
 
-                    if next_position == &self.end {
+                    if next_case.pos == self.end {
                         if next_score == min_score.0 {
-                            min_score.1.push(next_path.to_owned());
+                            min_score.1.extend(next_case.ids);
                         } else {
-                            min_score = (next_score, vec![next_path.to_owned()]);
+                            min_score = (next_score, next_case.ids);
                         }
                         continue;
                     }
 
-                    if matches!(maze[next_position.1][next_position.0], Cell::Visited(scores) if scores[direction_to_index(next_direction)] > next_score)
-                        || matches!(maze[next_position.1][next_position.0], Cell::Free)
+                    if matches!(maze[next_case.pos.1][next_case.pos.0], Cell::Visited(scores, _) if scores[direction_to_index(&next_case.dir)] > next_score)
+                        || matches!(maze[next_case.pos.1][next_case.pos.0], Cell::Free)
                     {
                         match cases.entry(next_score) {
                             Entry::Vacant(vacant_entry) => {
-                                vacant_entry.insert(vec![next_case.1]);
+                                vacant_entry.insert(vec![next_case]);
                             }
                             Entry::Occupied(mut occupied_entry) => {
-                                occupied_entry.get_mut().push(next_case.1);
+                                occupied_entry.get_mut().push(next_case);
                             }
                         }
                     }
@@ -158,7 +193,18 @@ impl ReindeerMaze {
             }
         }
 
-        min_score
+        let min_score_ids = HashSet::<usize>::from_iter(min_score.1.into_iter());
+        (
+            min_score.0,
+            maze
+                .iter()
+                .map(|line| {
+                    line.iter()
+                        .filter(|cell| matches!(cell, Cell::Visited(_, ids) if ids.iter().any(|id| min_score_ids.contains(id))))
+                        .count()
+                })
+                .sum::<usize>() + 1,
+        )
     }
 }
 
